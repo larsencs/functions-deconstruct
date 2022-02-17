f7ecfa30dd05dc2b14e4d86feed25e0f6a9366e1
larsencs <quietus77@gmail.com>
2022-02-17 11:25:10 -0600

    Initial commit

---
+++ main.js
@@ -0,0 +1,64 @@
+/**
+ * Drawing a picture:
+ * select pencil
+ * select paper
+ * scribble a rough draft
+ * select inks
+ * mix desired colors into new tubes
+ * select nib holder
+ * select nib
+ * burn coating off of nib
+ * attach nib to pen
+ * dip nib in ink
+ * poke paper with point of nib to produce dots
+ * repeat ad nauseum
+ */
+
+selectPencil = () =>{
+    console.log("Select desired pencil (preferrably a harder graphite)")
+}
+selectPaper = () =>{
+    console.log("Select desired paper size")
+}
+scribbleDraft = () =>{
+    console.log("Scribble a rough draft of the desired image on the page")
+}
+selectInk= () =>{
+    console.log("Select base ink colors to be used on the image")
+}
+mixInks = () =>{
+    console.log("Mix various tones from selected inks into different tubes")
+}
+selectNibHolder = () =>{
+    console.log("Select desired nib holder")
+}
+selectNib = () =>{
+    console.log("Select desired nib size")
+}
+attachNib = () =>{
+    console.log("Attach nib to nib holder")
+}
+burnNib = () =>{
+    console.log("Burn the factory coating off of the tip of the nib so that the ink flows freely")
+}
+dipPen = () =>{
+    console.log("Dip the nib in ink and scrape off excess")
+}
+stipple = () =>{
+    console.log("Lightly and repeatedly jab the paper with the tip of the nib producing small dots of varying density")   
+}
+repeat = () =>{
+    console.log("Repeat dipPen() and stipple() until image has been completed")
+ }
+ selectPencil()
+ selectPaper()
+ scribbleDraft()
+ selectInk()
+ mixInks()
+ selectNibHolder()
+ selectNib()
+ attachNib()
+ burnNib()
+ dipPen()
+ stipple()
+ repeat()
